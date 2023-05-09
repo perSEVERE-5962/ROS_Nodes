86fa80ac76c25a0d405dd169c7dcd0e2704b1b35
Programmer <programming@mvrobotics.net>
2023-05-09 19:25:01 -0400

    Added script to return greatest detected distance

--- src/crop_laser_scan.py
+++ src/crop_laser_scan.py
@@ -7,11 +7,14 @@ from math import pi
 ANGLE = pi/4
 cropped_pub = None
 
+
 def callback(msg):
     HALF_ANGLE=ANGLE/2
     index_count = int(HALF_ANGLE//msg.angle_increment)
+    #index_count = 3
     ranges = msg.ranges
-    right_ranges = ranges[len(msg.ranges)-index_count:]
+    #ranges = [2,3,4,5,3,2,3,2,1,7,8,5]
+    right_ranges = ranges[len(ranges)-index_count:]
     left_ranges = ranges[:index_count]
     res_ranges = np.concatenate((right_ranges, left_ranges), -1)
     #res_ranges = res_ranges[~np.isnan(res_ranges)]
@@ -19,6 +22,21 @@ def callback(msg):
 
     if cropped_pub:
         cropped_pub.publish(msg)
+
+    #find largest element
+    largest_val = 0
+    largest_idx = 0
+    for i in range(0, len(ranges) - 1):
+        if ranges[i] > largest_val:
+            largest_val = ranges[i]
+            largest_idx = i
+
+    print("largest distance: " + str(largest_val))
+    print("the index of ranges that it is located at: " + str(largest_idx))
+
+
+msg = None
+callback(msg)
 
 if __name__ =='__main__':
     try:
@@ -31,3 +49,5 @@ if __name__ =='__main__':
 
     except rospy.ROSInterruptException:
         pass
+
+
