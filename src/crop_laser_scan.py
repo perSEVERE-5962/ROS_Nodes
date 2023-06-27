5f1e65d38d707fddf18e621fa61987212ce794cf
Programmer <programming@mvrobotics.net>
2023-06-27 18:22:43 -0400

    Changed IP Address to match network

--- src/crop_laser_scan.py
+++ src/crop_laser_scan.py
@@ -26,7 +26,7 @@ def connectionListener(connected, info):
         notified[0] = True
         cond.notify()
 
-NetworkTables.initialize("10.248.240.237")
+NetworkTables.initialize("192.168.1.98")
 
 
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
