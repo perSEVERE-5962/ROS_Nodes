a6704b82fbd41da03367880246b85ae7682cd6d4
Programmer <programming@mvrobotics.net>
2023-06-27 19:13:51 -0400

    Added connection listener and initialization

--- src/crop_laser_scan.py
+++ src/crop_laser_scan.py
@@ -15,6 +15,28 @@ ANGLE = pi/4
 LEN_MAX = 100
 cropped_pub = None
 
+print(sys.byteorder)
+
+cond = threading.Condition()
+notified = [False]
+
+def connectionListener(connected, info):
+    print(info, '; Connected=%s' % connected)
+    with cond:
+        notified[0] = True
+        cond.notify()
+
+NetworkTables.initialize("192.168.1.98")
+
+
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+with cond:
+    print("Waiting")
+    if not notified[0]:
+        cond.wait()
+
+print("Connected!")
 table = NetworkTablesInstance.getDefault().getTable('laser_scan')
 
 
