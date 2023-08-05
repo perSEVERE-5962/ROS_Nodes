fb340a0363db87ff121e063f5afa8e7f26d4a4a1
Programmer <programming@mvrobotics.net>
2023-08-05 15:24:27 -0400

    Combined files

--- src/scan_2_net_tab.py
+++ src/scan_2_net_tab.py
@@ -8,8 +8,7 @@ from networktables import NetworkTables, NetworkTablesInstance
 from math import pi
 import sys
 
-
-
+from sklearn.linear_model import LinearRegression
 
 print(sys.byteorder)
 
@@ -84,18 +83,6 @@ def callback(msg):
 
     ranges = res_ranges
 
-    table.putNumberArray("ranges", ranges)
-
-
-    #get angle to move
-
-    angle_to_move = table.getNumber("angle_to_move", "[none returned]")
-    print("angle to move: ")
-    if isinstance(angle_to_move, str):
-        print(angle_to_move)
-    else:
-        print(str(angle_to_move*(180/pi)))
-
     # publish cropped scan
     current_time = rospy.Time.now()
     laser_scan.header.stamp = current_time
@@ -108,6 +95,74 @@ def callback(msg):
     laser_scan.range_max = 32.0
     laser_scan.ranges = ranges
 
+
+
+
+
+
+    #print("index number: " + str(len(ranges) - 1))
+    #print("total angle: " + str(ANGLE))
+
+    #get the exact angle increment after cropping
+    real_increment = ANGLE/(len(ranges) - 1)
+    #print("calculated index increment: " + str(real_increment))
+
+
+
+    #print(ranges)
+
+
+
+    vector_x = [0]*len(ranges)
+    vector_y = [0]*len(ranges)
+    for i in range(len(ranges)):
+        if np.isnan(ranges[i]):
+            ranges[i] = LEN_MAX
+            continue
+        angle = (real_increment * i) - HALF_ANGLE + (np.pi/2)
+
+        #print("index: " + str(i) + " value: " + str(ranges[i]) + " angle: " + str(angle))
+
+
+        #convert to (x, y)
+        x = np.cos(angle) * ranges[i]
+        y = np.sin(angle) * ranges[i]
+        vector_x[i] = x
+        vector_y[i] = y
+        #print("index: " + str(i) + " x: " + str(vector_x[i]) + " y: " + str(vector_y[i]))
+
+    #linear regression
+    array_x = np.asarray(vector_x).reshape((-1, 1))
+    array_y = np.asarray(vector_y)
+
+    model = LinearRegression()
+
+    model.fit(array_x, array_y)
+
+    r_sq = model.score(array_x, array_y)
+    #print("linear regression complete")
+    print("coefficient of determination: " + str(r_sq))
+    #print("intercept: " + str(model.intercept_))
+
+    slope = model.coef_[0]
+
+    print("slope: " + str(slope))
+
+
+    line_angle = np.arctan(slope)
+
+    angle_to_move = -line_angle
+    #print("angle to move the robot (in degrees): " + str(angle_to_move*180/pi))
+
+    #publish move angle to network table
+
+    print("angle to move: ")
+    if isinstance(angle_to_move, str):
+        print(angle_to_move)
+    else:
+        print(str(angle_to_move * (180 / pi)))
+
+    table.putNumber("angle_to_move", angle_to_move)
 
     pub.publish(laser_scan)
 
