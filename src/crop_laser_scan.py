3fd54b71187f43b030f0885cf7e54782c5e1c83d
Zachary Baron <baronzach962@gmail.com>
2023-08-08 18:47:07 -0400

    Fixed some of the code
    Including adding more clusters

--- src/crop_laser_scan.py
+++ src/crop_laser_scan.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import LaserScan
+import matplotlib
 import numpy as np
 from math import pi
+import math
 import pandas as pd
 import jenkspy
 from networktables import NetworkTables, NetworkTablesInstance
-import tkinter as tk
-from math import cos, tan
+from math import cos, tan, sin
+#Changing the Clustering Algo is: 
 NetworkTables.initialize("10.0.0.107")
-
 table = NetworkTablesInstance.getDefault().getTable('crop laser scan table')
-global intricate_switch
-root = tk.Tk()
-button = tk.Button(root, text="Switch", width=15, command=intricate_switch)
 ANGLE = pi/7 #originally 5 
 cropped_pub = None
-
+global angle
 def callback(msg):
     HALF_ANGLE=ANGLE/2
     index_count = int(HALF_ANGLE//msg.angle_increment)
-    ranges = msg.ranges #Collective of all points0
+    ranges = msg.ranges #of all points0
     right_ranges = ranges[len(msg.ranges)-index_count:]
     left_ranges = ranges[:index_count]
     res_ranges = np.concatenate((left_ranges, right_ranges), -1)
@@ -30,12 +28,12 @@ def callback(msg):
     for i in res_ranges:
         if i >= 0.9144:
             i = float("nan")
+    print(res_ranges)
     if cropped_pub:
         msg.ranges = res_ranges
         cropped_pub.publish(msg)
-
+    angle = -HALF_ANGLE
     angle_increment = msg.angle_increment
-    angle = angle
     new_list = []
     for i,z in enumerate(res_ranges):
         #new_list.append(x+angle_increment)
@@ -49,49 +47,54 @@ def callback(msg):
             answer = y*i
             second_new_list.append(answer)
             number+1
-    print(second_new_list)
     new_number = 0
     for i in res_ranges:
-        sine = sin(new_list[number])
+        sine = sin(new_list[new_number])
         new_answer = sine*i
         first_new_list.append(new_answer)
         new_number+1
-
-    import numpy as np
     from sklearn.cluster import KMeans
+    list_of_removal = []
     import matplotlib.pyplot as plt
-    import pandas as pd
-    kmeans = KMeans(n_clusters=4)
+    kmeans = KMeans(n_clusters=6)
+    #__
+    first_new_list = [item for item in first_new_list if not(math.isnan(item)) == True]
+    second_new_list = [item for item in second_new_list if not(math.isnan(item)) == True]   
+ 
+
+    #x = np.where(np.isnan(first_new_list))
+    #for i in x:
+     #   print(i)
+     #   list_of_removal.append(i+1)
+    #for i in list_of_removal:
+     #   pass
+        
+    #_________ first_new, second_new, new_list
     data = {
         "x": first_new_list,
         "y": second_new_list 
-    }
+        }
+    #_______________________
+    #______________________
     color = []
     df = pd.DataFrame(data)
     df["Specified_Category"] = kmeans.fit_predict(df)
     df["Specified_Category"] = df["Specified_Category"].astype("category")
     print(df)
     for i in df["Specified_Category"]:
-        print(i)
+        #print(i)
         if i == 0:color.append("red")
         elif i == 1:color.append("blue")
         elif i == 2:color.append("green")
         elif i == 3:color.append("black")
+        elif i == 4:color.append("purple")
+        elif i == 5:color.append("pink")
     plt.scatter(data["x"], data["y"], c=color)
-    plt.show()
-    
+    plt.savefig("clustering_graph.png")
 
+    #print(res_ranges, new_list)
+#Still Have to Fine Tune
 
-
-
-    #finding_pole(res_ranges)
-                #if i in right_ranges?
-                #move right until i = 0
-                #while i != 0:
-                #   move.right() send command for robot to move, and will auto stop when i == 0 (position of pole?) 
-		
-        #This is to get the pole points to pass into getting_calculations
-    
 
 
 
@@ -106,7 +109,3 @@ if __name__ =='__main__':
 
     except rospy.ROSInterruptException:
         pass
-
-#________________________________________________________________________________
-#Jenks Natural Breaks Optimization
-
